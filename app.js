fc6b956ae4389548fdb81834f55c341a13da9dd6
hieu-ng <hieutnguyen06@gmail.com>
2020-05-24 20:38:15 +1000

    Hide modal from outside click

--- app.js
+++ app.js
@@ -9,3 +9,9 @@ toggle.addEventListener('click' () => {
 
 open.addEventListener('click', () => modal.classList.add('show-modal'))
 close.addEventListener('click', () => modal.classList.remove('show-modal'))
+
+// Hide modal from outside click
+
+window.addEventListener('click', e => {
+	e.target == modal ? modal.classList.remove('show-modal') : false;
+})
