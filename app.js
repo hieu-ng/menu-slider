d91214aaa332ee8d8e801fe2f7732ff5ae7084bf
hieu-ng <hieutnguyen06@gmail.com>
2020-05-24 20:36:22 +1000

    Show modal

--- app.js
+++ app.js
@@ -6,3 +6,6 @@ const modal = document.getElementById('modal');
 toggle.addEventListener('click' () => {
 	document.body.classList.toggle('show-nav');
 })
+
+open.addEventListener('click', () => modal.classList.add('show-modal'))
+close.addEventListener('click', () => modal.classList.remove('show-modal'))
